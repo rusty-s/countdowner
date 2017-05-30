0c207fcb72e14caf1e4a3edafe1cc52825e34dae
Alex Raichev <araichev@users.noreply.github.com>
2017-05-30 12:21:59 +1200

    Fixed CLI

--- countdowner/cli.py
+++ countdowner/cli.py
@@ -4,18 +4,19 @@ import countdowner.main as m
 
 
 @click.command(short_help="Get the current prices of your watchlist products")
-@click.option('-d', '--domain', default=None,
+@click.option('-d', '--mailgun_domain', default=None,
     help="Mailgun domain key")
-@click.option('-k', '--key', default=None,
+@click.option('-k', '--mailgun_key', default=None,
     help="Mailgun API key")
 @click.option('-h', '--as_html', is_flag=True, default=True,
     help="Send the optional email as HTML if true; otherwise send it as text")
 @click.argument('watchlist_path')
 @click.argument('out_dir')
-def countdownit(watchlist_path, out_dir, domain=None, key=None, as_html=True):
+def countdownit(watchlist_path, out_dir, mailgun_domain=None, mailgun_key=None,
+  as_html=True):
     """
     Read a YAML watchlist located at ``--watchlist_path``, collect all the product information from Countdown, and write the result to a CSV located in the directory ``--out_dir``, creating the directory if it does not exist.
     If ``--domain`` (Mailgun domain) and ``--key`` (Mailgun API key) are given, then email the products on sale (if there are any) to the email address listed in the watchlist.
     """
-    m.run_pipeline(watchlist_path, out_dir, domain=domain, key=key, 
+    m.run_pipeline(watchlist_path, out_dir, mailgun_domain, mailgun_key, 
       as_html=as_html)
